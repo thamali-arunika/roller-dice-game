a0329b1594d8814ec3c4850c7a76fed1ccf5de10
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-11-17 14:57:12 +0530

    update

--- lib/get_started.dart
+++ lib/get_started.dart
@@ -7,7 +7,48 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:  Colors.grey[50],
-      body: ,
+      body:SafeArea(
+        child:Column(
+          children: [
+            Padding(
+             padding:const EdgeInsets.only(
+                left: 100.0,
+                right: 100.0,
+                top: 120,
+                bottom: 20,
+              ),
+              child: Image.asset( 'assets/images/getStart.png'),
+            ),
+             GestureDetector(
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return ();
+                  },
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color.fromARGB(255, 112, 91, 222),
+                ),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+          ],
+        ), 
+        ),
     );
   }
 }
