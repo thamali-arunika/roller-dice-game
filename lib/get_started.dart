28f5eb8674000543ff39b0a367969b9035e78c20
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-11-17 12:45:50 +0530

    create new componnt as get strat

---
+++ lib/get_started.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:  Colors.grey[50],
+      body: ,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,24 @@ void main() {
   runApp(const RollerDiceApp());
   // 
 }
+class GetStartedPage extends StatefulWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
 
+  @override
+  State<GetStartedPage> createState() => _GetStartedPageState();
+  
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroScreen(),
+      ),
+    );
+  }
+}
